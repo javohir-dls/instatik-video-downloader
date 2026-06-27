9435cf1065b94740b3be566d43b77475549f497f
javohir-dls <chorikichinov21@gmail.com>
2026-06-27 18:29:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
@@ -5,11 +6,14 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print("START ISHLADI")
-    await update.message.reply_text("OK ishlayapti")
+    await update.message.reply_text("Bot ishlayapti ✅")
 
 def main():
     print("BOT STARTED")
+
+    # 🔥 CRITICAL FIX FOR PYTHON 3.14
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
