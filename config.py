2b7b722db9e051da53004c953b6a192be1e1ac01
javohir-dls <chorikichinov21@gmail.com>
2026-06-27 16:27:39 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+BOT_TOKEN = "8787348134:AAFNAot8sCUcXfvibh-4kDrHjzRAWu2u5jQ"
+
+INSTAGRAM_URL = "https://instagram.com/javohir.ftbl"
+
+CAPTION = """
+✅ Video muvaffaqiyatli yuklandi!
+
+📢 @javohir.ftbl ga obuna bo'ling.
+"""
