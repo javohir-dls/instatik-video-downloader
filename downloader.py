7183043081104a66ab6a4b512be876bd85b2cbd2
javohir-dls <chorikichinov21@gmail.com>
2026-06-28 01:30:51 +0500

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,20 +1,28 @@
-import os
 import yt_dlp
+import os
 
-DOWNLOAD_FOLDER = "downloads"
+def download_video(url: str):
+    os.makedirs("downloads", exist_ok=True)
 
-if not os.path.exists(DOWNLOAD_FOLDER):
-    os.makedirs(DOWNLOAD_FOLDER)
-
-
-def download_video(url):
     ydl_opts = {
-        "outtmpl": f"{DOWNLOAD_FOLDER}/%(title)s.%(ext)s",
-        "format": "best",
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+        "outtmpl": "downloads/%(id)s.%(ext)s",
         "quiet": True,
-        "noplaylist": True,
+        "noplaylist": True
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
-        return ydl.prepare_filename(info)
+
+        if not info:
+            return None
+
+        file_path = ydl.prepare_filename(info)
+
+    file_path = os.path.splitext(file_path)[0] + ".mp4"
+
+    if os.path.exists(file_path):
+        return file_path
+
+    return None
