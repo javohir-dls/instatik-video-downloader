aa31f017b72c5a5cf43251d82aeb95e88657e1c0
javohir-dls <chorikichinov21@gmail.com>
2026-06-27 18:04:24 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,28 @@
-import asyncio
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
-from config import BOT_TOKEN
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    keyboard = [[InlineKeyboardButton("📢 Obuna", url="https://instagram.com/javohir.ftbl")]]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.message.reply_text("🎬 Bot ishlayapti", reply_markup=reply_markup)
-
 import yt_dlp
+import os
 
 def download_video(url):
     ydl_opts = {
         'format': 'mp4',
-        'outtmpl': '/tmp/video.mp4'
+        'outtmpl': 'video.mp4'
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
 
-async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def download(update, context):
     url = update.message.text
 
-    await update.message.reply_text("⏳ Video yuklanmoqda...")
+    await update.message.reply_text("⏳ Yuklanmoqda...")
 
     try:
         download_video(url)
-        await update.message.reply_video(video=open("/tmp/video.mp4", "rb"))
+
+        with open("video.mp4", "rb") as video:
+            await update.message.reply_video(video=video)
+
+        os.remove("video.mp4")
 
     except Exception as e:
         await update.message.reply_text(f"❌ Xatolik: {e}")
-    app = Application.builder().token(BOT_TOKEN).build()
-
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
-
-    print("Bot started")
-
-    # 🔥 CRITICAL FIX FOR RENDER + PYTHON 3.14
-    asyncio.set_event_loop(asyncio.new_event_loop())
-
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
