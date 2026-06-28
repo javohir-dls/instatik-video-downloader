6bb5f3cadcc12681477837c06770a44f4c9f32b1
javohir-dls <chorikichinov21@gmail.com>
2026-06-28 06:27:10 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
 import os
 
-# Bot token (Render ENV dan olinadi)
-BOT_TOKEN = os.getenv("8787348134:AAHIEBQ7VHZHDt80lw2SPAkhotuQpmy8eHM")
-
-# Majburiy obuna kanal
+TOKEN = os.getenv("8787348134:AAHIEBQ7VHZHDt80lw2SPAkhotuQpmy8eHM")
 CHANNEL = "@xushboqovblog"
-
-# Social links
-INSTAGRAM = "https://instagram.com/javohir.ftbl"
-TIKTOK = "https://tiktok.com"
-YOUTUBE = "https://youtube.com"
-SNAPCHAT = "https://snapchat.com"
-FACEBOOK = "https://facebook.com"
